efd4cb65385bb72c4dc53bd30080d4533bfb982d
elizasorensen <elizasorensen@ELIZASORENS79D5>
2024-03-08 16:43:57 -0700

    Finshed

--- mission9/src/App.tsx
+++ mission9/src/App.tsx
@@ -1,24 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import schoolData from './CollegeBasketballTeams.json';
+
+interface SchoolProps {
+  name: string;
+  school: string;
+  city: string;
+  state: string;
+}
+
+function Header() {
+  return (
+    <div>
+      <h1>March Madness</h1>
+      <h2>Here are all the NCAA Teams to be considered for the 2024 March </h2>
+    </div>
+  );
+}
+
+class School extends React.Component<SchoolProps> {
+  render() {
+    const { school, name, state, city } = this.props;
+
+    return (
+      <div
+        style={{
+          border: '2px solid black',
+          padding: '10px',
+          marginBottom: '10px',
+        }}
+      >
+        <h2>School Name: {school}</h2>
+        <h3>Mascot Name: {name}</h3>
+        <h3>
+          Location: {city}, {state}
+        </h3>
+      </div>
+    );
+  }
+}
+
+function SchoolList() {
+  const teams = schoolData.teams;
+  return (
+    <div>
+      {teams.map((schoolNum, index) => (
+        <School key={index} {...schoolNum} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <SchoolList />
     </div>
   );
 }
